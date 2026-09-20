e5f4f290bc2f7ccf72d44a67c12e9427ab6e2f7b
Jusn38 <yanzsmartwood@gmail.com>
2026-09-20 00:32:58 -0500

    Expose UI-ready capability catalog safely

--- src/pages/api/media/providers.ts
+++ src/pages/api/media/providers.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { requireFirebaseUser } from '../../../lib/firebaseAdmin';
+import { MEDIA_CAPABILITY_CATALOG } from '../../../lib/mediaProviders/capabilities';
 import { getAllProviderRuntimeStatuses } from '../../../lib/mediaProviders/registry';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -15,5 +16,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   return res.status(200).json({
     providers: getAllProviderRuntimeStatuses(),
+    capabilities: MEDIA_CAPABILITY_CATALOG,
   });
 }
