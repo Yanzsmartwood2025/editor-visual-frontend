bc5093746cabb7754bba0b02d7c1b342d64de5fc
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:26:38 -0500

    Generar: fix unused React imports

--- src/components/generar/api/3d/index.tsx
+++ src/components/generar/api/3d/index.tsx
@@ -1,3 +1,2 @@
-import React from 'react';
 import ModulePlaceholder from '../../ModulePlaceholder';
 export default function ApiThreeDModule(){return <ModulePlaceholder engine="api" module="3d" />;}
