2eb3e88e514d4abe4168bdff3bdaf81ef57c82b3
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:21:43 -0500

    Generar: add imagen/index.tsx

---
+++ src/components/generar/gpu/imagen/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import ModulePlaceholder from '../../ModulePlaceholder';
+export default function GpuImagenModule(){return <ModulePlaceholder engine="gpu" module="imagen" />;}
