241f8b1fda44d7dbd8fbdfd4fea85295b85f5fed
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:21:08 -0500

    Generar: add imagen/index.tsx

---
+++ src/components/generar/api/imagen/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import ModulePlaceholder from '../../ModulePlaceholder';
+export default function ApiImagenModule(){return <ModulePlaceholder engine="api" module="imagen" />;}
