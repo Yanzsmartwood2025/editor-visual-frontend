01ff98c211e98416519e33dc9eed2fd0146ac376
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:26:43 -0500

    Generar: fix unused React imports

--- src/components/generar/api/imagen/index.tsx
+++ src/components/generar/api/imagen/index.tsx
@@ -1,3 +1,2 @@
-import React from 'react';
 import ModulePlaceholder from '../../ModulePlaceholder';
 export default function ApiImagenModule(){return <ModulePlaceholder engine="api" module="imagen" />;}
