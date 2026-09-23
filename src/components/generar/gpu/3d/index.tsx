cc8f9de1fa64aa5a137852809282fb0d8b301dbf
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:26:49 -0500

    Generar: fix unused React imports

--- src/components/generar/gpu/3d/index.tsx
+++ src/components/generar/gpu/3d/index.tsx
@@ -1,3 +1,2 @@
-import React from 'react';
 import ModulePlaceholder from '../../ModulePlaceholder';
 export default function GpuThreeDModule(){return <ModulePlaceholder engine="gpu" module="3d" />;}
