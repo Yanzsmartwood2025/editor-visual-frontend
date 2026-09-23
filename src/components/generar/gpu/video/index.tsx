c546e68d929cff4ad9b80ebc64065163c01ed2e9
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:21:45 -0500

    Generar: add video/index.tsx

---
+++ src/components/generar/gpu/video/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import ModulePlaceholder from '../../ModulePlaceholder';
+export default function GpuVideoModule(){return <ModulePlaceholder engine="gpu" module="video" />;}
