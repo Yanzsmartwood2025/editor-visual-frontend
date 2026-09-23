2b202920db1f9b4509cafb715f7b0c4a11bf08b8
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:30:34 -0500

    Generar: fix GPU musica TypeScript import

--- src/components/generar/gpu/musica/index.tsx
+++ src/components/generar/gpu/musica/index.tsx
@@ -1,3 +1,2 @@
-import React from 'react';
 import ModulePlaceholder from '../../ModulePlaceholder';
 export default function GpuMusicaModule(){return <ModulePlaceholder engine="gpu" module="musica" />;}
