1b7c409591b84366cd165aa6a2db04c597723d83
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:21:40 -0500

    Generar: add 3d/index.tsx

---
+++ src/components/generar/api/3d/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import ModulePlaceholder from '../../ModulePlaceholder';
+export default function ApiThreeDModule(){return <ModulePlaceholder engine="api" module="3d" />;}
