eb3ff89e136c5fe8defb776bcc12beae47a39555
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:21:10 -0500

    Generar: add video/index.tsx

---
+++ src/components/generar/api/video/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import ModulePlaceholder from '../../ModulePlaceholder';
+export default function ApiVideoModule(){return <ModulePlaceholder engine="api" module="video" />;}
