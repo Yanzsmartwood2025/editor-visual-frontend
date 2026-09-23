5c7d9d279f2f27a9b9cd05c96632bf0a2d76bf94
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:30:38 -0500

    Generar: fix GPU video TypeScript import

--- src/components/generar/gpu/video/index.tsx
+++ src/components/generar/gpu/video/index.tsx
@@ -1,3 +1,2 @@
-import React from 'react';
 import ModulePlaceholder from '../../ModulePlaceholder';
 export default function GpuVideoModule(){return <ModulePlaceholder engine="gpu" module="video" />;}
