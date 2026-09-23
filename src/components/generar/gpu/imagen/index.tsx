acbbee2d8426258ded8d3ab2ae767783061a4d04
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:30:32 -0500

    Generar: fix GPU imagen TypeScript import

--- src/components/generar/gpu/imagen/index.tsx
+++ src/components/generar/gpu/imagen/index.tsx
@@ -1,3 +1,2 @@
-import React from 'react';
 import ModulePlaceholder from '../../ModulePlaceholder';
 export default function GpuImagenModule(){return <ModulePlaceholder engine="gpu" module="imagen" />;}
