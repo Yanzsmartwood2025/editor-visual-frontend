ea720eedd50731b0ac118bf711c37f7e8ec92db2
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:26:33 -0500

    Generar: fix unused React imports

--- src/components/generar/ModuleBreaker.tsx
+++ src/components/generar/ModuleBreaker.tsx
@@ -1,4 +1,4 @@
-import React, { Component, type ReactNode } from 'react';
+import { Component, type ReactNode } from 'react';
 
 type Props = { moduleKey: string; children: ReactNode; onReset?: () => void };
 type State = { failed: boolean };
