d9d93ba929a7e6bdff7a304a770bd6987ba71cef
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:26:47 -0500

    Generar: fix unused React imports

--- src/components/generar/api/video/index.tsx
+++ src/components/generar/api/video/index.tsx
@@ -1,3 +1,2 @@
-import React from 'react';
 import ModulePlaceholder from '../../ModulePlaceholder';
 export default function ApiVideoModule(){return <ModulePlaceholder engine="api" module="video" />;}
