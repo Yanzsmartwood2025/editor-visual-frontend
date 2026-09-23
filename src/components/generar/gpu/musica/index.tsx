dc4f5682afa2d4aa17ad00cffb29ecf5e129f0c0
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:21:51 -0500

    Generar: add musica/index.tsx

---
+++ src/components/generar/gpu/musica/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import ModulePlaceholder from '../../ModulePlaceholder';
+export default function GpuMusicaModule(){return <ModulePlaceholder engine="gpu" module="musica" />;}
