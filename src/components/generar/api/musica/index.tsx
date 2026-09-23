594b1188a03f54833c0c87efc6973e6cdbcc59c5
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:21:39 -0500

    Generar: add musica/index.tsx

---
+++ src/components/generar/api/musica/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import ModulePlaceholder from '../../ModulePlaceholder';
+export default function ApiMusicaModule(){return <ModulePlaceholder engine="api" module="musica" />;}
