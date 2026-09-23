02abc565c8cdf0b2193205b5da430347b9075d3d
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:21:47 -0500

    Generar: add audio/index.tsx

---
+++ src/components/generar/gpu/audio/index.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import ModulePlaceholder from '../../ModulePlaceholder';
+export default function GpuAudioModule(){return <ModulePlaceholder engine="gpu" module="audio" />;}
