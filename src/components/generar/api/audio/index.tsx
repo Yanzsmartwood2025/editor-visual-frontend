afe8d676264369688e6dbeb4350c20de6bbc4f9b
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:26:40 -0500

    Generar: fix unused React imports

--- src/components/generar/api/audio/index.tsx
+++ src/components/generar/api/audio/index.tsx
@@ -1,3 +1,2 @@
-import React from 'react';
 import ModulePlaceholder from '../../ModulePlaceholder';
 export default function ApiAudioModule(){return <ModulePlaceholder engine="api" module="audio" />;}
