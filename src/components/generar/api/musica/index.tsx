0b1032d9adb893928a95d80de225f99a0e16774d
Jusn38 <yanzsmartwood@gmail.com>
2026-09-23 16:26:45 -0500

    Generar: fix unused React imports

--- src/components/generar/api/musica/index.tsx
+++ src/components/generar/api/musica/index.tsx
@@ -1,3 +1,2 @@
-import React from 'react';
 import ModulePlaceholder from '../../ModulePlaceholder';
 export default function ApiMusicaModule(){return <ModulePlaceholder engine="api" module="musica" />;}
